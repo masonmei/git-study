b0c15dd0d77f05f8495a7805b82874d8fd8dfb25
Mason Mei <dongxu.m@gmail.com>
2012-10-18 15:41:53 +0800

    update branch1 again

--- git-study/src/org/personal/mason/git/NewBranch.java
+++ git-study/src/org/personal/mason/git/NewBranch.java
@@ -1,5 +1,15 @@
 package org.personal.mason.git;
 
 public class NewBranch {
+public static void main(String[] args) {
+	System.out.println("Update Branch1");
+	
+	System.out.println("Update Branch1 twice");
 
+	System.out.println("Update Branch1 again");
+	
+	System.out.println("Update Branch1 one more");
+	
+	System.out.println("Update Branch1 one more again");
+}
 }
