0ee4d3a6e41657c655ff1f9d9fefc0f5534addbd
Mason Mei <dongxu.m@gmail.com>
2012-10-18 15:27:10 +0800

    update branch 2

--- git-study/src/org/personal/mason/git/NewBranch2.java
+++ git-study/src/org/personal/mason/git/NewBranch2.java
@@ -3,5 +3,6 @@ package org.personal.mason.git;
 public class NewBranch2 {
 public static void main(String[] args) {
 	System.out.println("New Branch2!");
+	System.out.println("Update Branch2!");
 }
 }
