52ee5147099245c1fea4cd61cdd271974dc1eb7e
Mason Mei <dongxu.m@gmail.com>
2012-10-18 15:51:20 +0800

    Update Branch2 Third
    Signed-off-by: Mason Mei <dongxu.m@gmail.com>

--- git-study/src/org/personal/mason/git/NewBranch2.java
+++ git-study/src/org/personal/mason/git/NewBranch2.java
@@ -5,5 +5,6 @@ public static void main(String[] args) {
 	System.out.println("New Branch2!");
 	System.out.println("Update Branch2!");
 	System.out.println("Update Branch2 Second!");
+	System.out.println("Update Branch2 Third!");
 }
 }
